61a0ac5c6f8d41d580fb67e03c0c878a1305a2f7
happy <happy@example.com>
2023-02-20 09:12:09 -0800

    game server

---
+++ src/types/IUser.ts
@@ -0,0 +1,12 @@
+export interface IUserInfo {
+    id: number;
+    name: string;
+    email: string;
+    companyId?: string;
+    companyName?: string;
+}
+
+export interface UserInfo{
+    name: string;
+    roomId: string;
+}
